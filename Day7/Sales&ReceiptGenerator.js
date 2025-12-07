64850797b7a03dcbf42f0a14248969d18c969662
Victoria Holland <JavaDevVictoria@users.noreply.github.com>
2025-12-07 12:34:29 +0000

    Add stock checking functionality for products

--- Day7/Sales&ReceiptGenerator.js
+++ Day7/Sales&ReceiptGenerator.js
@@ -35,3 +35,21 @@ function generateReceipt(orderedItems) {
 }
 
 generateReceipt(orderedItems);
+
+const products = [
+    { product: "Laptop", stock: 5 },
+    { product: "Headphones", stock: 0 },
+    { product: "Smartphone", stock: 3 }
+];
+
+function checkStockLevels(products) {
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].stock > 0) {
+            console.log(`${products[i].product} is In Stock.`);
+        } else {
+            console.log(`${products[i].product} is Out of Stock.`);
+        }
+    }
+}
+
+checkStockLevels(products);
