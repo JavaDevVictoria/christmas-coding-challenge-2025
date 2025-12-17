4a1fcedc411aaf9e9c896de9ff56b337c34ba9f1
Victoria Holland <JavaDevVictoria@users.noreply.github.com>
2025-12-17 08:48:36 +0000

    SpringBoot Secure Auth App

---
+++ Day17/src/main/java/com/example/secureauthapp/model/User.java
@@ -0,0 +1,68 @@
+package com.example.secureauthapp.model;
+
+public class User {
+    private String username;
+    private String password;
+    private String role; // Add a role field
+    private String firstname;
+    private String lastname;
+    private String email;
+
+    public User(String username, String firstname, String lastname, String email, String password, String role) {
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getters and Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
